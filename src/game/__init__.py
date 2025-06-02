a46518fa17d41b860ee6f8c64d2a2a63edc5eaf4
kufanghua <fanghuaku.ee13@nycu.edu.tw>
2025-06-02 14:23:14 +0800

    Update __init__.py

--- src/game/__init__.py
+++ src/game/__init__.py
@@ -1,1 +1,18 @@
+"""
+遊戲核心模組
+包含遊戲管理器、地圖管理和波數管理
+"""
+
+from .game_manager import GameManager
+from .map_manager import MapManager, MapTile, TileType
+from .wave_manager import WaveManager, Wave
+
+__all__ = [
+    'GameManager',
+    'MapManager', 
+    'MapTile',
+    'TileType',
+    'WaveManager',
+    'Wave'
+]
 
